95ad7baf372afeb4f83bc0d4eb1841e9bfc91879
Егор Михайлович <81710799+grawwe@users.noreply.github.com>
2025-04-04 16:45:18 +0300

    Create backup.sh

---
+++ lab2/backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+backup_dir="/home/lab2/results"
+mkdir -p $backup_dir
+date_str=$(date +"%Y%m%d_%H%M%S")
+tar -czf "$backup_dir/backup_$date_str.tar.gz" /etc/network
+# Удаление старых бэкапов, оставляя только 10 последних
+ls -t $backup_dir/backup_*.tar.gz | tail -n +11 | xargs rm -f

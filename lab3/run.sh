3a2f76a399ecff0c907bfa8d4248cb180656afcc
Егор Михайлович <81710799+grawwe@users.noreply.github.com>
2025-04-04 16:50:47 +0300

    Create run.sh

---
+++ lab3/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/expect -f
+
+# === Параметры ===
+set SFTP_HOST "192.168.0.105"         ;# IP-адрес удаленного хоста
+set SFTP_USER "egor"               ;# Имя пользователя для подключения
+set SFTP_PASS "1234"                  ;# Пароль пользователя для подключения
+set REMOTE_DIR "/home/egor/"       ;# Директория на удаленном сервере
+set LOCAL_FILE "/home/egor/egor-sh/sftp.txt"            ;# Путь к локальному файлу для загрузки
+
+# === Проверка прав ===
+if {[catch {exec whoami} result]} {
+    puts "Этот скрипт нужно запускать с правами sudo!"
+    exit 1
+}
+
+# === Подключение через SFTP ===
+spawn sftp $SFTP_USER@$SFTP_HOST
+
+# Ожидание запроса на ввод пароля
+expect "password: "
+send "$SFTP_PASS\r"
+
+# Ожидание приглашения sftp
+expect "sftp> "
+
+# Загрузка файла на удаленный сервер
+send "put $LOCAL_FILE $REMOTE_DIR\r"
+
+# Завершение сессии
+expect "sftp> "
+send "exit\r"
+
+# Завершение скрипта
+expect eof
+puts "Файл успешно загружен через SFTP!"
